96a70f7c909043e96c129e6cd9b2489b54b378f5
Jeongbin Kang <kangbin8109@gmail.com>
2026-06-05 18:49:37 +0900

    feat: TextInput 에제 수정 및 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,20 +2,25 @@ import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 export default function Index() {
   const [text, setText] = useState("");
-  const [submitted, setSubmitted] = useState("");
+  const [submitted, setSubmmited] = useState("");
 
   return (
     <View style={styles.container}>
       {/* TextInput으로 입력받고 */}
+      {/* 버튼 누르면 submitted에 저장해서 보여줘요 */}
       <TextInput
         value={text}
         onChangeText={(value) => {
           setText(value);
         }}
-        placeholder="텍스트를 입력해주세요"
-      ></TextInput>
-      {/* 버튼 누르면 submitted에 저장해서 보여줘요 */}
-      <Button title="제출" onPress={() => setSubmitted(text)}></Button>
+        placeholder="텍스틀 입력해주세요."
+      />
+      <Button
+        title="제출"
+        onPress={() => {
+          setSubmmited(text);
+        }}
+      ></Button>
       <Text>{submitted}</Text>
     </View>
   );
