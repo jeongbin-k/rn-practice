07cd1d0de56fbf7a5b2ca7ba7225985d214d44c4
Jeongbin Kang <kangbin8109@gmail.com>
2026-06-10 17:54:11 +0900

    feat: useEffect + setInterval 타이머 구현 예제

--- components/practice/Practice7.tsx
+++ components/practice/Practice7.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
 
-export default function Practice7() {
+export default function Pracitce7() {
   const [count, setCount] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
@@ -13,14 +13,22 @@ export default function Practice7() {
         setCount((prev) => prev + 1);
       }, 1000);
 
+      // 메모리 누수
       return () => clearInterval(interval);
     }
   }, [isRunning]);
+
+  const reset = () => {
+    setCount(0);
+    setIsRunning(false);
+  };
+
   return (
     <View>
       <Text>{count}</Text>
       <Button onPress={() => setIsRunning(true)} title="시작"></Button>
       <Button onPress={() => setIsRunning(false)} title="정지"></Button>
+      <Button onPress={reset} title="초기화"></Button>
     </View>
   );
 }
